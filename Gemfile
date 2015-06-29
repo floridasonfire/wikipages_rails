0399225f8bc31cab9cc7d7b2370eabdd984cf999
Jessica Engel & Kim Dang <student@epicodus.com>
2015-06-29 16:24:02 -0700

    can add multiple phones

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'pry'
 gem 'rails'
 gem 'pg'
 gem 'sass-rails'
--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -8,10 +8,13 @@ class BusinessesController < ApplicationController
   def show
     @business = Business.find(params[:id])
   end
-  
+
   def create
     @category = Category.find(params[:category_id])
     @business = @category.businesses.new(business_params)
+    @business.phones = params[:phones]
+
+    binding.pry
     if @business.save
       redirect_to categories_path
     else
@@ -21,6 +24,6 @@ class BusinessesController < ApplicationController
 
   private
   def business_params
-    params.require(:business).permit(:name, :address, :website, :phones, :hours)
+    params.require(:business).permit(:name, :address, :website, :hours)
   end
 end
